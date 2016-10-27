1d94e03e8da6103c913b4e9f255626de8e4e2b9f
ZhongShengping <chdzsp@163.com>
2016-10-27 23:58:53 +0800

    Remove unused parameters
    The sqlite_package_name option has been useless,
    so delete it.
    
    Change-Id: Ibf1adc67235ddb5774eda5996408813b9df5c11d

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,11 +4,9 @@ class rally::params {
 
   case $::osfamily {
     'RedHat': {
-      $sqlite_package_name  = undef
       $package_name  = 'openstack-rally'
     }
     'Debian': {
-      $sqlite_package_name  = 'python-pysqlite2'
       $package_name  = 'rally'
     }
     default: {
