0c42b114e40df5ce53fe71dbdcf002d93dab8820
Takashi Kajinami <tkajinam@redhat.com>
2022-04-14 18:29:59 +0900

    Cinder: Support tuning parameters for backup feature
    Change-Id: I0154bcc2f4c4061c8b567ed9e84f2e1497fc0247

--- manifests/settings/cinder.pp
+++ manifests/settings/cinder.pp
@@ -25,21 +25,33 @@
 #   (Optional) Time to wait for cinder volume to be deleted.
 #   Defaults to $::os_service_default
 #
+# [*backup_restore_poll_interval*]
+#   (Optional) Interval between checks when waiting for backup restoring.
+#   Defaults to $::os_service_default
+#
+# [*backup_restore_timeout*]
+#   (Optional) Time to wait for cinder backup to be restored.
+#   Defaults to $::os_service_default
+#
 class rally::settings::cinder (
-  $volume_create_poll_interval = $::os_service_default,
-  $volume_create_prepoll_delay = $::os_service_default,
-  $volume_create_timeout       = $::os_service_default,
-  $volume_delete_poll_interval = $::os_service_default,
-  $volume_delete_timeout       = $::os_service_default,
+  $volume_create_poll_interval  = $::os_service_default,
+  $volume_create_prepoll_delay  = $::os_service_default,
+  $volume_create_timeout        = $::os_service_default,
+  $volume_delete_poll_interval  = $::os_service_default,
+  $volume_delete_timeout        = $::os_service_default,
+  $backup_restore_poll_interval = $::os_service_default,
+  $backup_restore_timeout       = $::os_service_default,
 ) {
 
   include rally::deps
 
   rally_config {
-    'openstack/cinder_volume_create_poll_interval': value => $volume_create_poll_interval;
-    'openstack/cinder_volume_create_prepoll_delay': value => $volume_create_prepoll_delay;
-    'openstack/cinder_volume_create_timeout':       value => $volume_create_timeout;
-    'openstack/cinder_volume_delete_poll_interval': value => $volume_delete_poll_interval;
-    'openstack/cinder_volume_delete_timeout':       value => $volume_delete_timeout;
+    'openstack/cinder_volume_create_poll_interval':  value => $volume_create_poll_interval;
+    'openstack/cinder_volume_create_prepoll_delay':  value => $volume_create_prepoll_delay;
+    'openstack/cinder_volume_create_timeout':        value => $volume_create_timeout;
+    'openstack/cinder_volume_delete_poll_interval':  value => $volume_delete_poll_interval;
+    'openstack/cinder_volume_delete_timeout':        value => $volume_delete_timeout;
+    'openstack/cinder_backup_restore_poll_interval': value => $backup_restore_poll_interval;
+    'openstack/cinder_backup_restore_timeout':       value => $backup_restore_timeout;
   }
 }
