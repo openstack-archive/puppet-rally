c66b61ea954583ad37973b075e9d1a4080e2059c
Takashi Kajinami <tkajinam@redhat.com>
2023-01-05 19:09:48 +0900

    Manila: Allow tuning for access creation/deletion polling
    This introduces support for a few tuning parameters which controls
    polling checks while creating or deleting share access.
    
    Change-Id: I47f98611201882c71ef781ed34cd43b80fffdbc0

--- manifests/settings/manila.pp
+++ manifests/settings/manila.pp
@@ -24,21 +24,45 @@
 #   (Optional) Timeout for Manila share deletion.
 #   Defaults to $::os_service_default
 #
+# [*access_create_poll_interval*]
+#   (Optional) Interval between checks when waiting for Manila access creation.
+#   Defaults to $::os_service_default
+#
+# [*access_create_timeout*]
+#   (Optional) Timeout for Manila access creation.
+#   Defaults to $::os_service_default
+#
+# [*access_delete_poll_interval*]
+#   (Optional) Interval between checks when waiting for Manila access deletion.
+#   Defaults to $::os_service_default
+#
+# [*access_delete_timeout*]
+#   (Optional) Timeout for Manila access deletion.
+#   Defaults to $::os_service_default
+#
 class rally::settings::manila (
-  $share_create_poll_interval = $::os_service_default,
-  $share_create_prepoll_delay = $::os_service_default,
-  $share_create_timeout       = $::os_service_default,
-  $share_delete_poll_interval = $::os_service_default,
-  $share_delete_timeout       = $::os_service_default,
+  $share_create_poll_interval  = $::os_service_default,
+  $share_create_prepoll_delay  = $::os_service_default,
+  $share_create_timeout        = $::os_service_default,
+  $share_delete_poll_interval  = $::os_service_default,
+  $share_delete_timeout        = $::os_service_default,
+  $access_create_poll_interval = $::os_service_default,
+  $access_create_timeout       = $::os_service_default,
+  $access_delete_poll_interval = $::os_service_default,
+  $access_delete_timeout       = $::os_service_default,
 ) {
 
   include rally::deps
 
   rally_config {
-    'openstack/manila_share_create_poll_interval': value => $share_create_poll_interval;
-    'openstack/manila_share_create_prepoll_delay': value => $share_create_prepoll_delay;
-    'openstack/manila_share_create_timeout':       value => $share_create_timeout;
-    'openstack/manila_share_delete_poll_interval': value => $share_delete_poll_interval;
-    'openstack/manila_share_delete_timeout':       value => $share_delete_timeout;
+    'openstack/manila_share_create_poll_interval':  value => $share_create_poll_interval;
+    'openstack/manila_share_create_prepoll_delay':  value => $share_create_prepoll_delay;
+    'openstack/manila_share_create_timeout':        value => $share_create_timeout;
+    'openstack/manila_share_delete_poll_interval':  value => $share_delete_poll_interval;
+    'openstack/manila_share_delete_timeout':        value => $share_delete_timeout;
+    'openstack/manila_access_create_poll_interval': value => $access_create_poll_interval;
+    'openstack/manila_access_create_timeout':       value => $access_create_timeout;
+    'openstack/manila_access_delete_poll_interval': value => $access_delete_poll_interval;
+    'openstack/manila_access_delete_timeout':       value => $access_delete_timeout;
   }
 }
